efa57ec8d430ba80337bbab00c343bf90ad6b178
kkorsch <kkorsch7@gmail.com>
2018-04-09 15:23:12 +0200

    FractalServicePRovider added

--- config/app.php
+++ config/app.php
@@ -151,6 +151,8 @@ return [
          * Package Service Providers...
          */
 
+         Spatie\Fractal\FractalServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
