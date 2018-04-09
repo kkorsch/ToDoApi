a97a112b6024e4bb5e319900340e6db95a9bd8ac
kkorsch <kkorsch7@gmail.com>
2018-04-09 16:02:53 +0200

    lists realtionship addeD

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
     ];
+
+    public function lists()
+    {
+      return $this->hasMany( List::class );
+    }
 }
