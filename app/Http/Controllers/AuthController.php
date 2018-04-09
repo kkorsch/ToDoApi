842bd174f76d2fb1b32224f73adaa37cb1860d60
kkorsch <kkorsch7@gmail.com>
2018-04-09 15:35:54 +0200

    register method createD

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
 
 class AuthController extends Controller
 {
-    //
+    public function register( RegisterRequest $request )
+    {
+      $user = new User;
+      $user->username = $request->username;
+      $user->email = $request->email;
+      $user->password = bcrypt( $request->password );
+
+      $user->save();
+
+      return fractal()->item( $user )->transformWith( new UserTransformer )->toArray();
+    }
 }
