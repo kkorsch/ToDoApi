eb269af5ce3df50933fe9e3d7bf7f83a55bfd299
kkorsch <kkorsch7@gmail.com>
2018-04-09 16:02:16 +0200

    user relationship added

--- app/Models/List.php
+++ app/Models/List.php
@@ -7,4 +7,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class List extends Authenticatable
 {
     protected $fillable = ['name'];
+
+    public function user()
+    {
+      return $this->belongsTo( User::class );
+    }
 }
