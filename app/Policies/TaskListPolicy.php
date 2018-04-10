cd6225cb9d565a797d8a5e06ad39bfaeaf01dab9
kkorsch <kkorsch7@gmail.com>
2018-04-10 13:22:28 +0200

    update method created

--- app/Policies/TaskListPolicy.php
+++ app/Policies/TaskListPolicy.php
@@ -3,19 +3,15 @@
 namespace App\Policies;
 
 use App\Models\User;
+use App\Models\TaskList;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class TaskListPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Create a new policy instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function update( User $user, TaskList $list )
     {
-        //
+      return $user->ownsList( $list );
     }
 }
