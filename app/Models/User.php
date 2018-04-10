318e0a8b056d6d059f373f63cb2e7ac51c5ff7a9
kkorsch <kkorsch7@gmail.com>
2018-04-10 12:21:21 +0200

    relationship name changed

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
         'password',
     ];
 
-    public function tasklists()
+    public function taskLists()
     {
        return $this->hasMany( TaskList::class );
     }
