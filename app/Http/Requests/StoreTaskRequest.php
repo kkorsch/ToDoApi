f1453b8561e98d02035723c1d652e9f014eeb619
kkorsch <kkorsch7@gmail.com>
2018-04-10 15:31:23 +0200

    rules added

--- app/Http/Requests/StoreTaskRequest.php
+++ app/Http/Requests/StoreTaskRequest.php
@@ -13,7 +13,7 @@ class StoreTaskRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class StoreTaskRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'body' => 'required|max:200',
+            'remind_at' => 'date',
         ];
     }
 }
