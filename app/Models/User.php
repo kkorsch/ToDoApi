bec08af7333c35d8348482917cd5634ee562ff3f
kkorsch <kkorsch7@gmail.com>
2018-04-10 13:32:13 +0200

    misspeling fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
         'password',
     ];
 
-    public function owsList( TaskList $list )
+    public function ownsList( TaskList $list )
     {
       return $list->user->id === $this->id;
     }
