727d8473fd8cf8928c96c3f3a56ed2bf0f9c80cc
kkorsch <kkorsch7@gmail.com>
2018-04-11 13:01:39 +0200

    store method createD

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
+use App\Models\TaskList;
 use Illuminate\Http\Request;
+use App\Transformers\TaskTransformer;
+use App\Http\Requests\StoreTaskReqeust;
 
 class TaskController extends Controller
 {
-    //
+    public function store( StoreTaskReqeust $request, TaskList $list )
+    {
+      $this->authorize( 'storeTask', $list );
+
+      $task = new Task;
+      $task->body = $request->body;
+      $task->remind_at = $request->get( 'remind_at', null );
+
+      $list->tasks()->save( $task );
+
+      return fractal()->item( $task )->transformWith( new TaskTransformer )->toArray();
+    }
 }
