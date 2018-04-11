f1d58e476e177b85a7795a8afd22c027688b6532
kkorsch <kkorsch7@gmail.com>
2018-04-11 12:37:53 +0200

    migration createD

---
+++ database/migrations/2018_04_11_102721_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('list_id')->unsigned()->index();
+            $table->text('body');
+            $table->timestamp('remind_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('list_id')->references('id')->on('task_lists')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
