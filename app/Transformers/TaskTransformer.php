7ba1d4ef8cfcac3c21320de8c97283101f48ddea
kkorsch <kkorsch7@gmail.com>
2018-04-11 14:23:58 +0200

    remind_at updated

--- app/Transformers/TaskTransformer.php
+++ app/Transformers/TaskTransformer.php
@@ -9,10 +9,12 @@ class TaskTransformer extends TransformerAbstract
 {
   public function transform( Task $task )
   {
+    $remind_at = $task->remind_at ? $task->remind_at->toDateString() : $task->remind_at;
+    
     return [
       'id' => $task->id,
       'body' => $task->body,
-      'remind_at' => $task->remind_at->toDateString(),
+      'remind_at' => $remind_at,
       'created_at' => $task->created_at->toDateString(),
       'created_at_humans' => $task->created_at->diffForHumans(),
     ];
