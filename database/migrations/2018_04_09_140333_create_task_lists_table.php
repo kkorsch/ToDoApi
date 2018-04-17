d32d4117c86cf47dbc67008e103f72b9bbb5b71a
kkorsch <kkorsch7@gmail.com>
2018-04-17 13:42:18 +0200

    migration renamed

--- database/migrations/2018_04_09_140333_create_task_lists_table.php
+++ database/migrations/2018_04_09_140333_create_task_lists_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateListsTable extends Migration
+class CreateTaskListsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateListsTable extends Migration
      */
     public function up()
     {
-        Schema::create('lists', function (Blueprint $table) {
+        Schema::create('task_lists', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned()->index();
             $table->string('name');
